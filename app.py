03c8be81b7c45969d383d6bd73f25d61d3033810
Vineet Vijaykumar <mysticvin@gmail.com>
2025-05-30 09:42:34 -0700

    pep8 compliance app.py

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from pprint import pprint
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     project_id = 85
     sequence_data = get_sequence_query_results(project_id)
     pprint(sequence_data)
     return render_template("sequences.html", sequences=sequence_data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
